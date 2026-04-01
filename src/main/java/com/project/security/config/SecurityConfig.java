f58ae7819ec16ffee1c9b2b3c5a23a9e4a411032
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-04-01 11:09:48 -0300

    feat: create security config

---
+++ src/main/java/com/project/security/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.project.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http){
+        http.
+//              desabilitando a config padrão do spring
+                csrf(csrf -> csrf.disable())
+//                criando meu próprio padrão
+                .authorizeHttpRequests(auth -> auth
+//                rotas que serão permitidas
+                .requestMatchers("/users","/users/**").permitAll()
+//                permissões dos usuários
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+// Função pra encriptografar a senha
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
