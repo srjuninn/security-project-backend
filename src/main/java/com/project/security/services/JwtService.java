954e6fc9560543bf170a3a72fcc2cac8f6138a94
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-04-22 08:03:31 -0300

    feat: add jwt variables

--- src/main/java/com/project/security/services/JwtService.java
+++ src/main/java/com/project/security/services/JwtService.java
@@ -3,19 +3,38 @@ package com.project.security.services;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.Data;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 @Data
 
 public class JwtService {
+//    criação do token de segurança
 
-    private final String SECRET = "doyourjumpssonnobodyyesdoorthedadisawesomethereisnoway";
+    @Value("${jwt.secret}")
+    private String SECRET;
+    @Value("${jwt.expiration}")
+    private long EXPIRATION;
 
     public String generateToken(String email){
+        long now = System.currentTimeMillis();
         return Jwts.builder()
                 .setSubject(email)
+                .setIssuedAt(new Date(now))
+                .setExpiration(new Date(now + EXPIRATION))
                 .signWith(SignatureAlgorithm.HS256, SECRET)
                 .compact();
+    }
+//      validação do token
+    public String validateEmail(String token){
+        return Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
     }
 }
